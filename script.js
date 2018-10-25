c6aec58d41ac97b9d94201c8854c98c10246033b
Annica Alienus <anbj1001@student.miun.se>
2018-10-25 10:45:27 +0200

    pushar

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-function validateEmail(email) 
-{
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-}
+$(function () {
+
+    $('#contactusform').submit(function (e) {
+        e.preventDefault();
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var comment = $("#comment").val();
+
+        $(".error").remove();
+
+        if (name.length < 1) {
+            $("#name").before('<span class="error">Obligatiriskt fält</span>');
+        }
+        if (comment.length < 1) {
+            $("#comment").before('<span class="error">Obligatiriskt fält</span>');
+        }
+        if (email.length < 1) {
+            $("#email").before('<span class="error">Obligatiriskt fält</span>');
+        } else {
+            var regEx = /\S+@\S+\.\S+/;
+            var validEmail = regEx.test(email);
+            if (!validEmail) {
+                $("#email").before('<span class="error">Ogiltlig e-mail adress</span>');
+            }
+        }
+    });
+});
